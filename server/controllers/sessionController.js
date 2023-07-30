2d91ec2017af57cc1ae09f9273a74b8582893880
Sam Heck <heck.sam@gmail.com>
2023-07-29 18:07:39 -0700

    finished sessions, signup, login, update-stats, tested with postman.
    Co-authored-by: Janica Abagat abagat07@gmail.com
    Co-authored-by: Daniel Kim danyk.code@gmail.com

--- server/controllers/sessionController.js
+++ server/controllers/sessionController.js
@@ -1,11 +1,39 @@
 const sessionController = {};
+const Session = require('../models/sessionModel.js')
 
 sessionController.startSession = (req, res, next) => {
     // will be called after user has been authenticated. will be receiving user._id.$
     // use session documents in db for now - look at other options after this is setup
     // create a new session document who has a cookieId set to users id. 
+    if (!res.locals._id) return next({
+        log: 'error in sessionController.StartSession',
+        message: {
+          err: `Error user id undefined`
+        }})
+    Session.create({ cookieId: res.locals._id }).then(user => {
+        return next();
+    }).catch(err => next({
+        log: 'error in sessionController.StartSession',
+        message: {
+          err: `Error creating session in db: ${err}`
+        }}));
 }
 
 sessionController.isLoggedIn = (req, res, next) => {
     // verify if user has an ssid cookie and if they have an active session in db.
+    Session.findOne({ cookieId: req.cookies.ssid }) 
+    .then((session) => {
+      if(!session) return res.redirect('/');
+      else {
+        return next()
+      }
+    })
+    .catch((err) => {
+      return next({
+        log: 'Error occurred in sessionController.isLoggedIn',
+        message: {err: 'An error occurred'},
+      });
+    });
 }
+
+module.exports = sessionController;
---
+++ server/models/sessionModel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const sessionSchema = new Schema({
+    cookieId: { type: String, required: true, unique: true },
+    createdAt: { type: Date, expires: 30, default: Date.now }
+});
+
+
+module.exports = mongoose.model('session', sessionSchema);
